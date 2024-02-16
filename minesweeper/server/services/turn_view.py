cc5c14d4c204569a134c4e810f940b4a00f4d4a9
k-mickey <kokmickey@yandex.ru>
2024-02-16 13:42:48 +0300

    rewrite next_turn from recurse to loop. some fixes

--- minesweeper/server/services/turn_view.py
+++ minesweeper/server/services/turn_view.py
@@ -2,9 +2,8 @@ from typing import Set, List, NamedTuple
 
 from django.shortcuts import get_object_or_404
 
-from server.models import Game
+from server.models import Game, Mine
 from server.services.create_fields import _get_coordinates
-from server.settings import MAX_WIDTH_GRID, MAX_HEIGHT_GRID
 
 
 class Coord(NamedTuple):
@@ -12,11 +11,11 @@ class Coord(NamedTuple):
     y_height: int
 
 
-def get_game(validated_data: dict) -> Game:
+def get_game_model(validated_data: dict) -> Game:
     game_id = validated_data.get('game_id')
     game = get_object_or_404(
-        Game.objects.select_related('mines'),
-        game_id=game_id
+        Game.objects.prefetch_related('mines'),
+        game_id__exact=game_id
     )
     return game
 
@@ -26,37 +25,42 @@ def update_game(game: Game, validated_data: dict) -> Game:
 
     coordinates = _get_coordinates(game)
     if new_coord in coordinates:
-        game = game_over(game, coordinates)
+        game = _game_over(game, coordinates)
     else:
-        game = next_turn(game, new_coord)
+        game = _next_game_turn(game, new_coord)
     game.save()
     return game
 
 
-def game_over(game: Game, coordinates: Set[Coord]) -> Game:
+def _game_over(game: Game, coordinates: Set[Coord]) -> Game:
     game.completed = True
-    game.field = show_field(game, coordinates)
+    game.field = _open_field(game, coordinates)
     return game
 
 
-def next_turn(game: Game, new_coord: Coord) -> Game:
+def _next_game_turn(game: Game, new_coord: Coord) -> Game:
     field = game.field
     field_mines = game.field_mines
-    cell_field = field_mines[new_coord.x_width][new_coord.y_height]
 
-    field[new_coord.x_width][new_coord.y_height] = cell_field
-    for x in range(new_coord.x_width - 1, new_coord.x_width + 2):
-        for y in range(new_coord.y_height - 1, new_coord.y_height + 2):
-            is_new_coord = x == new_coord.x_width and y == new_coord.y_height
-            is_valid_coord = 0 <= x < MAX_WIDTH_GRID and 0 <= y < MAX_HEIGHT_GRID
-            if not is_new_coord and is_valid_coord:
-                return next_turn(game, Coord(x, y))
+    stack_coordinates = [new_coord]
+    visited_coordinates = set()
+
+    while stack_coordinates:
+        coord = stack_coordinates.pop()
+        if coord in visited_coordinates or not _is_valid_coord(coord, game):
+            continue
+
+        visited_coordinates.add(coord)
+        cell_field = _add_cell_to_field(field, field_mines, coord)
+        if cell_field == 0:
+            _add_neighbor_coordinates(stack_coordinates, coord)
 
     game.field = field
     return game
 
 
-def show_field(game: Game, coordinates: Set[Coord], is_win: bool = True) -> List[List[str | int]]:
+def _open_field(game: Game, coordinates: Set[Coord], is_win: bool = True) \
+        -> List[List[str | int]]:
     if is_win:
         return game.field_mines
 
@@ -64,3 +68,24 @@ def show_field(game: Game, coordinates: Set[Coord], is_win: bool = True) -> List
     for coord in coordinates:
         field_mines[coord.x_width][coord.y_height] = 'X'
     return field_mines
+
+
+def _is_valid_coord(coord: Coord, game: Game) -> bool:
+    return 0 <= coord.x_width < game.width and \
+    0 <= coord.y_height < game.height
+
+
+def _add_cell_to_field(
+        field: List[List[str | int]],
+        field_mines: List[List[str | int]],
+        coord: Coord
+) -> int:
+    cell_field = field_mines[coord.x_width][coord.y_height]
+    field[coord.x_width][coord.y_height] = cell_field
+    return cell_field
+
+
+def _add_neighbor_coordinates(stack_coordinates: List[Coord], coord: Coord):
+    for x in range(coord.x_width - 0, coord.x_width + 2):
+        for y in range(coord.y_height - 0, coord.y_height + 2):
+            stack_coordinates.append(Coord(x, y))
