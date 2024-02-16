57280607310cde124af69727cb3ee7aed5aa9013
k-mickey <kokmickey@yandex.ru>
2024-02-16 13:43:24 +0300

    add atomic. some fixes

--- minesweeper/server/serializers.py
+++ minesweeper/server/serializers.py
@@ -1,35 +1,34 @@
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MinValueValidator
+from django.db import transaction
 from rest_framework import serializers
 
 from .models import Game
 from .services.create_fields import create_field, create_mines, create_field_mines
-from .settings import MIN_WIDTH_GRID, MAX_WIDTH_GRID, MIN_HEIGHT_GRID, MAX_HEIGHT_GRID
 
 
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
-        fields = '__all__'
+        fields = ['width', 'height', 'mines_count']
 
     def create(self, validated_data: dict) -> Game:
-        game = Game.objects.create(**validated_data)
-        game.field = create_field(validated_data)
-        create_mines(game, validated_data)
-        create_field_mines(game)
-        return game
+        with transaction.atomic():
+            game = Game.objects.create(**validated_data)
+            game.field = create_field(validated_data)
+            create_mines(game, validated_data)
+            create_field_mines(game, validated_data)
+            return game
 
 
 class TurnViewSerializer(serializers.Serializer):
     game_id = serializers.CharField()
     col = serializers.IntegerField(
         validators=[
-            MinValueValidator(MIN_WIDTH_GRID),
-            MaxValueValidator(MAX_WIDTH_GRID),
+            MinValueValidator(0),
         ]
     )
     row = serializers.IntegerField(
         validators=[
-            MinValueValidator(MIN_HEIGHT_GRID),
-            MaxValueValidator(MAX_HEIGHT_GRID),
+            MinValueValidator(0),
         ]
     )
--- minesweeper/server/services/create_fields.py
+++ minesweeper/server/services/create_fields.py
@@ -1,8 +1,8 @@
 import random
 from typing import List, Set, Tuple, NamedTuple
 
+from django.db import transaction
 from server.models import Game, Mine
-from server.settings import MAX_WIDTH_GRID, MAX_HEIGHT_GRID
 
 
 class Coord(NamedTuple):
@@ -16,6 +16,7 @@ def create_field(validated_data: dict, default_cell: str | int = ' ') -> List[Li
     return [[default_cell] * height for _ in range(width)]
 
 
+@transaction.atomic
 def create_mines(game: Game, validated_data: dict) -> None:
     mines_coordinates = _get_mine_coordinates(validated_data)
     for x_width, y_height in mines_coordinates:
@@ -26,9 +27,9 @@ def create_mines(game: Game, validated_data: dict) -> None:
         )
 
 
-def create_field_mines(game: Game) -> None:
+def create_field_mines(game: Game, validated_data: dict) -> None:
     coordinates = _get_coordinates(game)
-    field_mines = create_field(game, 0)
+    field_mines = create_field(validated_data, 0)
     for coord in coordinates:
         field_mines = _add_around_cnt(coord, coordinates, field_mines)
         field_mines[coord.x_width][coord.y_height] = 'M'
@@ -39,15 +40,14 @@ def create_field_mines(game: Game) -> None:
 
 def _add_around_cnt(coord: Coord, coordinates: Set[Coord], field: List[List[int]]) -> int:
     x_min = max(0, coord.x_width - 1)
-    x_max = min(coord.x_width + 2, MAX_WIDTH_GRID - 1)
+    x_max = min(coord.x_width + 2, len(field))
     y_min = max(0, coord.y_height - 1)
-    y_max = min(coord.y_height + 2, MAX_HEIGHT_GRID - 1)
+    y_max = min(coord.y_height + 2, len(field[0]))
 
     for x_width in range(x_min, x_max):
         for y_height in range(y_min, y_max):
             if Coord(x_width, y_height) in coordinates:
                 continue
-
             cell = field[x_width][y_height]
             field[x_width][y_height] = cell + 1
     return field
@@ -67,4 +67,4 @@ def _get_mine_coordinates(validated_data: dict) -> Set[Tuple[int, int]]:
 
 
 def _get_coordinates(game: Game) -> Set[Coord]:
-    return {Coord(coord.x_width, coord.y_height) for coord in game.mines}
+    return {Coord(coord.x_width, coord.y_height) for coord in game.mines.all()}
