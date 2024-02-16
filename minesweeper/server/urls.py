e1c3e6398cc70f64ebb13e6fc1204b54dd1c61c3
k-mickey <kokmickey@yandex.ru>
2024-02-16 13:43:40 +0300

    some fix

--- minesweeper/server/urls.py
+++ minesweeper/server/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('new', views.new),
+    path('turn', views.turn),
 ]
--- minesweeper/server/views.py
+++ minesweeper/server/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .serializers import GameSerializer, TurnViewSerializer
-from .services.turn_view import get_game, update_game
+from .services.turn_view import get_game_model, update_game
 
 
 @api_view(['POST'])
@@ -18,7 +18,7 @@ def turn(request):
     serializer = TurnViewSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
 
-    game = get_game(serializer.validated_data)
+    game = get_game_model(serializer.validated_data)
     game = update_game(game, serializer.validated_data)
 
     return Response(GameSerializer(game).data)
